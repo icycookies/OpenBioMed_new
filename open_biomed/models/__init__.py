d6461a8efa48b612877c784f6427493df7e9972f
tinyswine <wen_luo@mfe.berkeley.edu>
2024-12-31 04:13:46 +0000

    [Feature] New models added: BioT5 and BioT5+, added prompt for BioT5+

--- open_biomed/models/__init__.py
+++ open_biomed/models/__init__.py
@@ -1,13 +1,21 @@
 from open_biomed.models.foundation_models.molt5 import MolT5
+from open_biomed.models.foundation_models.biot5 import BioT5
+from open_biomed.models.foundation_models.biot5_plus import BioT5_PLUS
 
 MODEL_REGISTRY = {
     "text_based_molecule_editing": {
-        "molt5": MolT5
+        "molt5": MolT5,
+        "biot5": BioT5,
+        "biot5_plus": BioT5_PLUS
     },
     "molecule_captioning": {
-        "molt5": MolT5
+        "molt5": MolT5,
+        "biot5": BioT5,
+        "biot5_plus": BioT5_PLUS
     },
     "text_guided_molecule_generation": {
-        "molt5": MolT5
+        "molt5": MolT5,
+        "biot5": BioT5,
+        "biot5_plus": BioT5_PLUS
     }
 }
